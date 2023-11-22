2595d7c9d84868d76f7eb604130753ac6e2807e3
zhens297 <zhens297@newschool.edu>
2023-11-22 13:42:23 -0500

    entry 9 update

---
+++ entries/entry9/script.js
@@ -0,0 +1,28 @@
+$("#tulip").click(function(){
+  alert("everyday may not be good, but there is good in every day. i wish you all the best.");
+});
+
+$("#tulip2").click(function(){
+  alert("i have a deep love for flowers. pink tulips specifically represent caring and good wishes— this is a bouquet i hope expresses some form of care.");
+});
+
+$("#tulip3").click(function(){
+  alert("i hope today brings you your favourite weather!");
+});
+
+$("#tulip4").click(function(){
+  alert("the internet can be a very tiring place. feel free to rest here a while. have a drink of water. get up and stretch. close your eyes. this is a gentle reminder to take a break!");
+});
+
+$("#tulip5").click(function(){
+  alert("have more faith in yourself. the fact that you are still standing here, today, is proof enough of your strength.");
+});
+
+$("#tulip6").click(function(){
+  alert("you are worth it. you are the most worth it person in your whole entire life. you are deserving of everything and more. realize this soon.");
+});
+
+$("#tulip7").click(function(){
+  alert("there is joy in the smallest of things. a smile exchanged with a stranger. a single flower amongst concrete. the temperature of your morning coffee being just right. happiness is more than grand actions of joy, it is everywhere around you.");
+});
+
